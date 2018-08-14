ff08e35adc2ac449652bac767f13d1d9e756d894
sotrteacher <42185871+sotrteacher@users.noreply.github.com>
2018-08-13 20:33:43 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/** main.c - Este es un archivo Hola Mundo
+ * incluido en este curso --solo para evitar 
+ * la maldici\'on del programador--. En este curso, 
+ * Usted aprender\'a a usar correctamente muchos de 
+ * los elementos del lenguaje C. Especialmente, usted 
+ * deber\'a a´render a usar apuntadores, de lo contrario
+ * sufrir\'a lo indecible y un perro salvaje lo 
+ * perseguir\'a por los pasillo de la UPIITA (si no 
+ * aprende a usar bien los apuntadores). 
+ */
+#include <stdio.h> /* int printf(char*, ...) 
+Este es un ejemplo de un comentario de bloque.
+*/
+#include <stdlib.h>//  Comentario de una sola l\'inea.
+
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  putchar('\n');
+  printf("HOLA MUNDO C!!!\n");
+  system("PAUSE");	
+  return 0;
+}
